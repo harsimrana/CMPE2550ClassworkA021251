ed55e0730fed5a24875487d04889adf508b22deb
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-09-24 08:47:53 -0600

    Updated Demo 03

---
+++ demos/Demo03/index.php
@@ -0,0 +1,22 @@
+<?php
+    require_once './libPHP/db.php';
+
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Demo 03: Working with Database</title>
+    </head>
+    <body>
+       <h1> Demo 03: Working with Database</h1>
+
+       <div>
+         
+       </div>
+    </body>
+</html>
--- demos/Demo03/libPHP/db.php
+++ demos/Demo03/libPHP/db.php
@@ -1,28 +1,34 @@
 <?php
-    $mysql_connetion = null;
+    $mysql_connection = null;
     $mysql_response = "";
     $mysql_status ="";
 
     mysqlconnect();  // Calling function to test connection
+    /***********************************************************
+     * mysqlconnect(): Establish connection to DB
+     * Inputs        : N/a
+     * Output        : n/a [error log]
+     ***********************************************************/
 
     function mysqlconnect()
     {
-        global $mysql_connetion, $mysql_response, $mysql_status;
+        global $mysql_connection, $mysql_response, $mysql_status;
 
         /*
         Database Name: aulakhha_1251_2550_A02
         Username: aulakhha_Admin2550A02
         Password: M^9RdcPngGHhlKKM
         */
-        $mysql_connetion = new mysqli("localhost",              // Server Name
+        //mysqli(): will connect to DB and return connection information
+        $mysql_connection = new mysqli("localhost",              // Server Name
                                       "aulakhha_Admin2550A02", // Username
-                                      "M^9RdcPngGHhlKKM",      // Password
+                                      "M^9RdcPngGHhlKKM",      // Password : Make sure there is no . in your password
                                       "aulakhha_1251_2550_A02");// Database Name
 
-        if($mysql_connetion->connect_error)
+        if($mysql_connection->connect_error)
         {
-            $mysql_response = "Connect Error ( ". $mysql_connetion->connect_errno . ") ".
-                               $mysql_connetion -> connect_error;
+            $mysql_response = "Connect Error ( ". $mysql_connection->connect_errno . ") ".
+                               $mysql_connection -> connect_error;
             error_log( json_encode($mysql_response));
 
         }
@@ -32,6 +38,36 @@
             error_log("Connection successfully ");
         }                              
 
+    }
+
+    /*********************************************
+     *  mysqlQuery() : To execute your select queries
+     *  Inputs       :  Query [string]
+     *  Output        : Result set or false
+     *********************************************/
+    function mysqlQuery( $query )
+    {
+       global $mysql_connection, $mysql_response, $mysql_status;
+       
+       $result = false;
+
+       // Checking the connection
+       if($mysql_connection == null)
+       {
+            error_log("No database connection");
+
+            $mysql_response = "No database connection";
+            return $result;
+
+       }
+       // Run the query 
+       // query function will return result set or false
+       else if( !($result = $mysql_connection -> query( $query )) ) // query function will execute your query
+       {
+            $mysql_response = " Query Error { $mysql_connection->errno } : {$mysql_connection ->error} ";
+       }
+
+       return $result; 
     }
 
 ?>
